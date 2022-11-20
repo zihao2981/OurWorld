59a305dbfab25b74033683934e32ebb5443b3e0a
lyndon <zihao2981@outlook.com>
2022-11-20 16:13:01 +0800

    初始化项目

---
+++ src/main/java/com/lyndon/ourworld/OurWorldApplication.java
@@ -0,0 +1,13 @@
+package com.lyndon.ourworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurWorldApplication.class, args);
+    }
+
+}
